446937e1c2d9eea9f8b680fb4a6699c3841841ac
TeamRaiderz <nikninza@icloud.com>
2017-05-30 21:42:06 +0300

    Add project files.

---
+++ RulettiSystem/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RulettiSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Program.winNumber = Program.getWinningNumber();
+            String winColor = Program.getWinningColor();
+
+            this.label5.Text = "Tulos: " + winColor + "(" + Program.winNumber + ")";
+        }
+    }
+    }
---
+++ RulettiSystem/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RulettiSystem
+{
+    static class Program
+    {
+
+        public static int winNumber = 0;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+       public static int getWinningNumber()
+        {
+            Random random = new Random();
+
+            double encoded = random.NextDouble();
+            Console.WriteLine("Encoded: " + encoded);
+            double winNumFloat = encoded * 15;
+            int winNum = (int)Math.Round(winNumFloat);
+
+            if(winNum >= 15)
+            {
+                winNum = 14;
+            }
+
+            Console.WriteLine("Win Number: " + winNum + " (" + winNumFloat + ")");
+            return winNum;
+        }
+
+        public static String getWinningColor()
+        {
+            String r = "Red";
+            String b = "Black";
+            String g = "Green";
+
+            if (winNumber > 7 && winNumber != 0)
+            {
+                return b;
+            }
+            else if (winNumber <= 7 && winNumber != 0)
+            {
+                return r;
+            }
+            else if (winNumber == 0)
+            {
+                return g;
+            }
+
+            return "";
+        }
+
+    }
+}
